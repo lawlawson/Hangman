1be23f58105578031d34c0008a6f08a478873381
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2020-07-08 23:32:32 +0100

    Added rules for selected words and wrong letters

--- script.js
+++ script.js
@@ -39,6 +39,24 @@ function displayWord() {
 window.addEventListener('keydown', e => {
   if(e.keyCode >= 65 && e.keyCode <= 90) {
     const letter = e.key;
+
+    if(selectedWord.includes(letter)){
+      if(!correctLetters.includes(letter)){
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if(!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
   }
 });
 
